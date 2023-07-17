9b293f7bf2e3acfd706b63a74ac8db8dd7a694e6
Giorgio5i6 <giorgio.cartechini@gmail.com>
2023-07-17 10:35:28 -0400

    Added preamble with licence

--- standalone/main.cc
+++ standalone/main.cc
@@ -1,3 +1,22 @@
+// *************************************************************************
+// * MONAS is a C++ package that calculates cell surviavl curvs and        *
+// * dose dependednt RBE from microdosimetric spectra.			   *
+// *									   *
+// * Copyright © 2023 Giorgio Cartechini <giorgio.cartechini@miami.edu>	   *
+// * 									   *
+// * This program is free software: you can redistribute it and/or modify  *
+// * it under the terms of the GNU General Public License as published by  *
+// * the Free Software Foundation, either version 3 of the License, or     *
+// * (at your option) any later version.			           *
+// * 									   *
+// * This program is distributed in the hope that it will be useful,       *
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+// * GNU General Public License for more details.			   *
+// * 									   *
+// * You should have received a copy of the GNU General Public license     *
+// * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+// **************************************************************************
 #include<iostream>
 #include<vector>
 #include<cstring>
